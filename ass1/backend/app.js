6ec2e60ee7b2aa5e76eb1eeea5578d9bfb74cce6
ZoroZero <40843946+ZoroZero@users.noreply.github.com>
2020-08-14 09:26:04 +0700

    Update app.js

--- ass1/backend/app.js
+++ ass1/backend/app.js
@@ -1,10 +1,21 @@
 const express = require('express')
+var request = require('request')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+app.get('/users', (req, res) => {
+    request('https://reqres.in/api/users', function(error, response, body){
+      if(!error && response.statusCode == 200){
+        res.send(body)
+      }
+    }
+)})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
